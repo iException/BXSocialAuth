a51b224091951e28ddd8347bda98a61be0eb2677
Yiming Tang <yimingnju@gmail.com>
2016-02-29 15:33:04 +0800

    Cleanup tests

---
+++ Example/Tests/BXSocialAuthTests.m
@@ -0,0 +1,20 @@
+//
+//  BXSocialAuthTests.m
+//  BXSocialAuthTests
+//
+//  Created by Yiming Tang on 02/22/2016.
+//  Copyright (c) 2016 Yiming Tang. All rights reserved.
+//
+
+// https://github.com/Specta/Specta
+
+#import <BXSOcialAuth/BXSocialAuth.h>
+
+SpecBegin(BXSocialAuth)
+
+describe(@"BXSocialAuth", ^{
+
+});
+
+SpecEnd
+
